91f9acccfc1dc88776e31eea61bd295ef8c5c283
catabada <huynhhuuan10121972@gmail.com>
2023-04-21 14:42:58 +0700

    initial project

---
+++ src/main/java/vn/edu/hcmuaf/fit/dacn_booking_health_api/DacnBookingHealthApiApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.hcmuaf.fit.dacn_booking_health_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DacnBookingHealthApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DacnBookingHealthApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vn/edu/hcmuaf/fit/dacn_booking_health_api/DacnBookingHealthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.hcmuaf.fit.dacn_booking_health_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DacnBookingHealthApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
